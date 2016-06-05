45ad2a297c657e7a59eb487ef4cc2422a1782198
bnossum <bnossum@gmail.com>
2016-06-05 09:34:03 +0200

    m

--- icestick_uart.v
+++ icestick_uart.v
@@ -1,5 +1,7 @@
 /* Top level that just instantiates a UART in loopback mode in an icestick.
  * Assumtions: 12M clock. 115200 bps. 8N1 format.
+ * Commented out instantiations of uart_m were used to examine generated code in 
+ * Synplify
  * 
  * LogicCells:
  * 38 for uart proper
@@ -75,11 +77,50 @@ module top
    SB_LUT4 #( .LUT_INIT(16'haaaa))
    cmb( .O(c_rxpinmeta1), .I3(1'b0), .I2(1'b0), .I1(1'b0), .I0(rxpinmeta1));
    SB_DFF metareg( .Q(rxpin), .C(clk), .D(c_rxpinmeta1));
+
+   //                                                                               Divider
+   //                                                                               | Nr LUTS
+//   uart_m #(.SYSCLKFRQ(10*8*6),  .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.0000 37
+//   uart_m #(.SYSCLKFRQ(10*4*13), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.5000 38
+//   uart_m #(.SYSCLKFRQ(10*2*25), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.2500 40
+//   uart_m #(.SYSCLKFRQ(10*2*27), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.7500 40
+
+//   uart_m #(.SYSCLKFRQ(10*1*49), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.1250 41
+//   uart_m #(.SYSCLKFRQ(10*1*51), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.3750 41
+//   uart_m #(.SYSCLKFRQ(10*1*53), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.6250 41
+//   uart_m #(.SYSCLKFRQ(10*1*55), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.8750 41
+//   uart_m #(.SYSCLKFRQ(5*1*97 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.0625 42
+//   uart_m #(.SYSCLKFRQ(5*1*99 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.1875 42
+//   uart_m #(.SYSCLKFRQ(5*1*101), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.3125 42
+//   uart_m #(.SYSCLKFRQ(5*1*103), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.4375 42
+//   uart_m #(.SYSCLKFRQ(5*1*105), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.5625 42
+//   uart_m #(.SYSCLKFRQ(5*1*107), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.6875 42
+//   uart_m #(.SYSCLKFRQ(5*1*109), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.8125 42
+//   uart_m #(.SYSCLKFRQ(5*1*111), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 6.9375 42
    
-//   uart_m #(.SYSCLKFRQ(12000000), .BITCLKFRQ(115200), 
-//            .ACCEPTEDERROR_IN_PERCENT(20), .HASRXBYTEREGISTER(0) )
-   uart_m #(.SYSCLKFRQ(8*2), .BITCLKFRQ(1), 
-            .ACCEPTEDERROR_IN_PERCENT(20), .HASRXBYTEREGISTER(0) )
+//   uart_m #(.SYSCLKFRQ(10*8*1 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.0000 33
+//   uart_m #(.SYSCLKFRQ( 5*1*17), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.0625 40
+//   uart_m #(.SYSCLKFRQ(10*1*9 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.1250 39
+//   uart_m #(.SYSCLKFRQ( 5*1*19), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.1875 40
+//   uart_m #(.SYSCLKFRQ(10*2*5 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.2500 38
+//   uart_m #(.SYSCLKFRQ( 5*1*21), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.3175 40
+//   uart_m #(.SYSCLKFRQ(10*1*11), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.3750 39
+//   uart_m #(.SYSCLKFRQ( 5*1*23), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.4375 40
+//   uart_m #(.SYSCLKFRQ(10*4*3 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.5000 36
+//   uart_m #(.SYSCLKFRQ( 5*1*25), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.5625 40
+//   uart_m #(.SYSCLKFRQ(10*1*13), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.6250 39
+//   uart_m #(.SYSCLKFRQ( 5*1*27), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.6875 40
+//   uart_m #(.SYSCLKFRQ(10*2*7 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.7500 38
+//   uart_m #(.SYSCLKFRQ( 5*1*29), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.8125 40
+//   uart_m #(.SYSCLKFRQ(10*1*15), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.8750 39
+//   uart_m #(.SYSCLKFRQ( 5*1*31), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 1.9375 40
+//   uart_m #(.SYSCLKFRQ(10*8*2 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 2.0000 34
+
+//   uart_m #(.SYSCLKFRQ(10*8*9 ), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 9.0 37
+//   uart_m #(.SYSCLKFRQ(10*4*19), .BITCLKFRQ(10), .ACCEPTEDERROR_IN_PERCENT(1)) // 9.5 39
+
+   uart_m #(.SYSCLKFRQ(12000000), .BITCLKFRQ(115200), .ACCEPTEDERROR_IN_PERCENT(2)) // 13 38
+   
    uart
      (/*AUTOINST*/
       // Outputs
